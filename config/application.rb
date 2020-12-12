ed6b514506cd902bd21a947f0aec9b9e327973f7
江添貴之 <>
2020-12-12 15:00:31 +0900

    application.rb の設定

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,23 @@ module WonderfulEditor
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    # 以下、追加分
+    config.generators do |g|
+      g.template_engine false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       routing_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       request_specs: true
+    end
+
+    config.api_only = true
+
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    # config.generators.system_tests = nil
   end
 end
